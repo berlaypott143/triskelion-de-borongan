ab5133aeeb89a2b8e4986da08e5b2099961a71c9
berlaypott143 <aldrinjaylej@gmail.com>
2024-10-11 01:37:19 +0800

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const triskelionDB = require("./triskelionDB");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+	res.send("<h1>HELLO FROM SABANG TRISKELION</h1>");
+	console.log("hello from backend");
+});
+
+app.get("/data", async (req, res) => {
+	try {
+		const result = await triskelionDB.query(
+			"SELECT * FROM sabang_community_chapter"
+		);
+		res.json(result.rows);
+	} catch (err) {
+		console.error("Error executing query", err.stack);
+		res.status(400).send("Database query error");
+	}
+});
+
+app.listen(PORT, () => {
+	console.log(`Server is listening on http://localhost:${PORT}`);
+});
---
+++ backend/triskelionDB.js
@@ -0,0 +1,26 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+// Configure the PostgreSQL connection details using environment variables
+const pool = new Pool({
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+});
+
+pool.connect((err, client, release) => {
+	if (err) {
+		return console.error("Error acquiring client", err.stack);
+	}
+	client.query("SELECT NOW()", (err, result) => {
+		release();
+		if (err) {
+			return console.error("Error executing query", err.stack);
+		}
+		console.log("Successful connection", result.rows);
+	});
+});
+
+module.exports = pool;
